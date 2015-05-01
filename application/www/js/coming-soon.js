12b3625c3b575ed64696c5bc208c525d32ccac32
Tom <tom@obdstudios.com>
2015-05-01 14:08:49 -0600

    Removing coming soon links.

--- application/www/js/coming-soon.js
+++ application/www/js/coming-soon.js
@@ -9,7 +9,7 @@ $(document).ready(function(e) {
 	
 	/*Countdown
 	*******************************************/
-	$('#timer').countdown('2015/8/10', function(event) {
+	$('#timer').countdown('2015/5/10', function(event) {
     $(this).html(event.strftime('%D:%H:%M:%S'));
   });
 	
