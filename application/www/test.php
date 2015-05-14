18d47463c4ab630a36aad071f1505ffb51e4755c
James Jensen <james@obdstudios.com>
2015-05-14 15:14:38 -0600

    modify email to work on webfaction

--- application/www/test.php
+++ application/www/test.php
@@ -3,4 +3,10 @@ $to = 'james@obdstudios.com';
 $subject = 'Test Message';
 $message = 'Hello, World!';
 $headers = "From: noreply@sagecart.org\r\n"; // Or sendmail_username@hostname by default
-mail($to, $subject, $message, $headers);
+$result = mail($to, $subject, $message, $headers);
+
+if($result) {
+    echo "success";
+} else {
+    echo "failure";
+}
